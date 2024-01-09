86add4f5bcb7a22037a36bc594fdce979d019dc2
John <johnf@solidappmaker.com>
2024-01-09 12:43:59 +0530

    new end point add

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ require('dotenv').config()
 const port=process.env.PORT
 
 const app=express()
-
+const data={"name":"Pankaj"}
 
 app.get("/",(req,res)=>{
     res.end("hello")
 })
+
+app.get("/data",(req,res)=>{
+    res.json(data)
+})
+
 
 
 app.listen(port,()=>{
